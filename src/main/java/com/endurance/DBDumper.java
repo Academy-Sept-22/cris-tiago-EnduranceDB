8893f1a103f22a119c9c778115dd8b159d5c73c2
CrisMouta <crismouta@gmail.com>
2022-10-18 16:35:12 +0200

    we are retrieving data from DB correctly!!!

--- src/main/java/com/endurance/DBDumper.java
+++ src/main/java/com/endurance/DBDumper.java
@@ -3,13 +3,15 @@ package com.endurance;
 import com.endurance.db.DBConnection;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DBDumper {
 
     public void dumpDatabase() throws Exception {
 
         try (Connection connection = DBConnection.createConnection("localhost", 3306,
-                "endurance", "root", "password")) {
+                "endurance", "root", "secret")) {
 
             dumpCountry(connection);
 
@@ -17,9 +19,16 @@ public class DBDumper {
 
     }
 
-    private void dumpCountry(Connection connection) {
-
-
+    private void dumpCountry(Connection connection) throws Exception{
+        Statement statement = connection.createStatement();
+        String sql = ("SELECT * FROM endurance.Country;");
+        ResultSet result = statement.executeQuery(sql);
+        System.out.println("Country Table:");
+        System.out.println("Name:");
+        while (result.next()) {
+            String countryValue = result.getString("Name");
+            System.out.println(countryValue);
+        }
     }
 
     public static void main(String[] args) {
